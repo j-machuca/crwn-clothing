98195ec8e0114024870c6fb2e9c4ae2c58e2f8d9
Jorge Machuca <jmachuca06@gmail.com>
2023-02-11 22:18:52 -0300

    add to cart functionality added

--- src/components/product-card/product-card.component.js
+++ src/components/product-card/product-card.component.js
@@ -1,13 +1,26 @@
-import "./product-card.styles.scss";
+import { useContext } from "react";
 
 import Button from "../button/button.component";
 
+import { CartContext } from "../../context/cart.context";
+
+import "./product-card.styles.scss";
+
 const ProductCard = ({ product }) => {
   const { name, price, imageUrl } = product;
+
+  const { addItemToCart } = useContext(CartContext);
+
+  const addProductToCart = () => {
+    addItemToCart(product);
+  };
+
   return (
     <div className="product-card-container">
       <img src={imageUrl} alt={name} />
-      <Button buttonType="inverted">Add to Cart</Button>
+      <Button buttonType="inverted" onClick={addProductToCart}>
+        Add to Cart
+      </Button>
       <div className="footer">
         <span className="name">{name}</span>
         <span className="price">{price}</span>
--- src/components/routes/navigation/navigation.component.js
+++ src/components/routes/navigation/navigation.component.js
@@ -21,7 +21,7 @@ import { signOutUser } from "../../../utils/firebase/firebase.utils";
 
 const Navigation = () => {
   const { currentUser } = useContext(UserContext);
-  const { isCartOpen, setIsCartOpen } = useContext(CartContext);
+  const { isCartOpen } = useContext(CartContext);
 
   return (
     <>
