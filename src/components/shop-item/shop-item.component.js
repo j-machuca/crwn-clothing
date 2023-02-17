1ba27698d532927275ca4e4fdc2bc4552a25dd4f
Jorge Machuca <jorge_machuca@hotmail.com>
2023-02-17 13:40:51 -0300

    finished 141

--- src/components/shop-item/shop-item.component.js
+++ src/components/shop-item/shop-item.component.js
@@ -1,9 +1,13 @@
 import { ShopItemContainer, Body, BackgroundImage } from "./shop-item.styles";
+import { useNavigate } from "react-router-dom";
 
 const ShopItem = ({ category }) => {
-  const { title, imageUrl } = category;
+  const { title, imageUrl, route } = category;
+  const navigate = useNavigate();
+
+  const onNavigateHandler = () => navigate(route);
   return (
-    <ShopItemContainer>
+    <ShopItemContainer onClick={onNavigateHandler}>
       <BackgroundImage imageUrl={imageUrl} />
       <Body>
         <h2>{title}</h2>
