bbe3f7fd08d0aa7efd8a2724e18dce6fe42a0519
Jorge Machuca <jmachuca06@gmail.com>
2023-02-21 18:25:42 -0300

    added reselect and memoization to category selector

--- src/store/categories/category.selector.js
+++ src/store/categories/category.selector.js
@@ -1,6 +1,25 @@
-export const selectCategoriesMap = (state) =>
+import { createSelector } from "reselect";
+
+const selectCategoryReducer = (state) => state.categories;
+
+export const selectCategories = createSelector(
+  [selectCategoryReducer],
+  (categoriesSlice) => {
+    return categoriesSlice.categories;
+  }
+);
+
+export const selectCategoriesMap = createSelector([selectCategories], (state) =>
   state.categories.categories.reduce((acc, category) => {
     const { title, items } = category;
     acc[title] = items;
     return acc;
-  }, {});
+  }, {})
+);
+
+// export const selectCategoriesMap = (state) =>
+//   state.categories.categories.reduce((acc, category) => {
+//     const { title, items } = category;
+//     acc[title] = items;
+//     return acc;
+//   }, {});
